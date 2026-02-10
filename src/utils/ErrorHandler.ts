ce466a985108203943ba324854ebeefccfb7df84
Moda20 <kadhem03@gmail.com>
2026-02-10 20:41:38 +0100

    updating api error to return json instead of text errors
    Note : Added a custom APIError for better error categorisation. usage will depend on future updates

--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -1,5 +1,23 @@
-export class BadRequestError extends Error {
-  constructor(public message: string = "Bad Request") {
-    super(message);
+import type { StatusCodes } from "http-status-codes";
+
+export class APIError extends Error {
+  public readonly message: string;
+  public readonly httpCode?: StatusCodes;
+  public readonly defaultCode: number = 400;
+
+  constructor(
+    message: string,
+    name?: string,
+    options?: ErrorOptions,
+    httpCode?: StatusCodes,
+  ) {
+    super(message, options);
+
+    this.httpCode = httpCode ?? this.defaultCode;
+    this.message = message;
+    this.name = name ?? "APIError";
+
+    Object.setPrototypeOf(this, APIError.prototype);
+    Error.captureStackTrace(this);
   }
 }
